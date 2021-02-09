6051ba51292d3b37f99c07b1a2d5d9f7f5203222
João Pedro Martins <joaopedromsn@gmail.com>
2021-02-08 23:26:47 -0300

    init server connection

---
+++ admin/service/client-service.js
@@ -0,0 +1,12 @@
+console.log('data')
+const http = new XMLHttpRequest()
+
+http.open('GET', 'http://localhost:3000/profile')
+http.send()
+
+
+
+http.onload = ()  => {
+    const data = http.response
+    console.log(data)
+}
