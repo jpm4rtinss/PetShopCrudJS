1de8387ff5d439f9151e4a25907bddbffc29be9b
João Pedro Martins <joaopedromsn@gmail.com>
2021-02-10 00:21:44 -0300

    display customer data

--- admin/service/client-service.js
+++ admin/service/client-service.js
@@ -1,11 +1,11 @@
 console.log('data')
-const creatNewLine = (name, email) => {
+const createNewLine = (name, email) => {
    const lineNewClient  = document.createElement('tr')
   const content = `<td class="td" data-td>${name}</td>
   <td>${email}</td>
   <td>
       <ul class="tabela__botoes-controle">
-          <li><a href="../telas/edita_cliente.html?id=${id}" class="botao-simples botao-simples--editar">Editar</a></li>
+          <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
           <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
       </ul>
   </td> `
@@ -13,6 +13,9 @@ const creatNewLine = (name, email) => {
   lineNewClient.innerHTML = content
   return lineNewClient
 }
+
+const table = document.querySelector('[data-tabela')
+
  
 
 
@@ -25,6 +28,8 @@ http.send()
 
 
 http.onload = ()  => {
-    const data = http.response
-    console.log(data)
+    const data = JSON.parse(http.response)
+    data.forEach(
+        elemento => {table.appendChild(createNewLine(elemento.nome, elemento.email))});
+   
 }
