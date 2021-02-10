7528fe8f94cad108c82038dd8a9fb7221f0f69e9
João Pedro Martins <joaopedromsn@gmail.com>
2021-02-09 23:55:10 -0300

    line template

--- admin/service/client-service.js
+++ admin/service/client-service.js
@@ -1,4 +1,18 @@
 console.log('data')
+const creatNewLine = () => {
+  const content = `<td class="td" data-td>${nome}</td>
+  <td>${email}</td>
+  <td>
+      <ul class="tabela__botoes-controle">
+          <li><a href="../telas/edita_cliente.html?id=${id}" class="botao-simples botao-simples--editar">Editar</a></li>
+          <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+      </ul>
+  </td> `
+}
+ 
+
+
+
 const http = new XMLHttpRequest()
 
 http.open('GET', 'http://localhost:3000/profile')
